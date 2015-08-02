6a03a99940d2bb1c7c030772f0bae71619220875
Rodolfo Martinez <rodolfo.martinez@mail.utoronto.ca>
2015-08-01 23:26:54 -0400

    everything workin

--- flite/public/js/filter.js
+++ flite/public/js/filter.js
@@ -2,63 +2,63 @@ var flite = angular.module('Flite', ['parse-angular']);
 
 
 flite.run(function($rootScope) {
-    Parse.initialize("TYM8QwWiHHFHBogeIk8N2UahFS5p9fvoLpRjG5LL",
-        "3iOvI03egswlVQn5K1WMCLdLQJbjHxoAoYN7o3Wm");
+  Parse.initialize("TYM8QwWiHHFHBogeIk8N2UahFS5p9fvoLpRjG5LL",
+    "3iOvI03egswlVQn5K1WMCLdLQJbjHxoAoYN7o3Wm");
 
-    if(Parse.User.current() != null){
-        $rootScope.sessionUser = Parse.User.current();
-    }
+  if(Parse.User.current() != null){
+    $rootScope.sessionUser = Parse.User.current();
+  }
 
-    window.fbAsyncInit = function() {
+  window.fbAsyncInit = function() {
         Parse.FacebookUtils.init({ // this line replaces FB.init({
             appId      : '792544870853072', // Facebook App ID
             cookie     : true,  // enable cookies to allow Parse to access the session
             xfbml      : true,  // initialize Facebook social plugins on the page
             version    : 'v2.4' // point to the latest Facebook Graph API version
-        });
+          });
     // Run code after the Facebook SDK is loaded.
-    };
+  };
 
-    (function(d, s, id){
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) {return;}
-        js = d.createElement(s); js.id = id;
-        js.src = "//connect.facebook.net/en_US/sdk.js";
-        fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
+  (function(d, s, id){
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {return;}
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
 });
 
-flite.controller('UserForm', function($rootScope){
+flite.controller('UserForm', function($rootScope, $http){
 
-    this.user = new Parse.User();
+  this.user = new Parse.User();
 
-    this.signUp = function(){
-        var temp = this;
+  this.signUp = function(){
+    var temp = this;
 
-        var User = Parse.Object.extend("_User");
-        var user = new User();
+    var User = Parse.Object.extend("_User");
+    var user = new User();
 
-        user.set("username", this.user.name);
-        user.set("password", this.user.password);
-        user.set("email", this.user.email);
-        user.set("phoneNumber", this.user.phone);
-        user.set("creditNumber", this.user.credit);
-        user.set("admin", this.user.admin);
+    user.set("username", this.user.name);
+    user.set("password", this.user.password);
+    user.set("email", this.user.email);
+    user.set("phoneNumber", this.user.phone);
+    user.set("creditNumber", this.user.credit);
+    user.set("admin", false);
 
-        user.signUp(null, {
-          success: function(user) {
-            $rootScope.sessionUser = Parse.User.current();
-            alert("Welcome! Now link your Facebook account please");
-          },
-          error: function(item, error) {
-            console.log('Failed to create new object, with error code: ' + error.message);
-          }
-        });
-    }
+    user.signUp(null, {
+      success: function(user) {
+        $rootScope.sessionUser = Parse.User.current();
+        alert("Welcome! Now link your Facebook account please");
+      },
+      error: function(item, error) {
+        console.log('Failed to create new object, with error code: ' + error.message);
+      }
+    });
+  }
 
-    this.logIn = function(){
-        Parse.User.logIn(this.user.name, this.user.password, {
-          success: function(user) {
+  this.logIn = function(){
+    Parse.User.logIn(this.user.name, this.user.password, {
+      success: function(user) {
             // Do stuff after successful login.
             $rootScope.sessionUser = Parse.User.current();
             window.location.href = "index.html";
@@ -67,81 +67,77 @@ flite.controller('UserForm', function($rootScope){
             // The login failed. Check error to see why.
           }
         });
-    };
+  };
 
-    this.fLink = function() {
-        if (!Parse.FacebookUtils.isLinked($rootScope.sessionUser)) {
-          Parse.FacebookUtils.link($rootScope.sessionUser, null, {
-            success: function(user) {
-              alert("Succesfully linked with Facebook!");
-              window.location.href = "index.html";
-            },
-            error: function(user, error) {
-              alert(error.message);
-            }
-          });
+  this.fLink = function() {
+    if (!Parse.FacebookUtils.isLinked($rootScope.sessionUser)) {
+      Parse.FacebookUtils.link($rootScope.sessionUser, null, {
+        success: function(user) {
+          alert("Succesfully linked with Facebook!");
+          window.location.href = "index.html";
+        },
+        error: function(user, error) {
+          alert(error.message);
         }
-    };
-
-    this.logOut = function(){
-        Parse.User.logOut();
-        $rootScope.sessionUser = Parse.User.current();
+      });
     }
+  };
+
+  this.logOut = function(){
+    Parse.User.logOut();
+    $rootScope.sessionUser = Parse.User.current();
+  }
 });
 
 flite.controller('Logistics', function ($rootScope){
-    this.sender={user: $rootScope.sessionUser, reciever: null, from: "",
-      to:"", byWhen: new Date(), item:null};
+  this.sender={user: $rootScope.sessionUser, reciever: null, from: "",
+  to:"", byWhen: new Date(), item:null};
 
-    this.reciever={name: "", phone:"", address: ""};
+  this.reciever={name: "", phone:"", address: ""};
 
-    this.item={user: $rootScope.sessionUser, name: "", size: 1, weight: 1, value:1, price: 0};
+  this.item={user: $rootScope.sessionUser, name: "", size: 1, weight: 1, value:1, price: 0};
 
-    var temp = this;
+  var temp = this;
 
-    this.save= function(){
-        var Item = Parse.Object.extend("Item");
-        var item = new Item();
-        item.set("name", this.item.name);
-        item.set("user", this.item.user);
-        item.set("size", this.item.size);
-        item.set("weight", this.item.weight);
-        item.set("value", this.item.value);
-        item.set("price", this.item.price);
-
-
-        var Sender = Parse.Object.extend("Senders");
-        var sender = new Sender();
-        sender.set("user", this.sender.user);
-
-        sender.set("from", this.sender.from);
-        sender.set("to", this.sender.to);
-        sender.set("byWhen", this.sender.byWhen);
+  this.save= function(){
+    var Item = Parse.Object.extend("Item");
+    var item = new Item();
+    item.set("name", this.item.name);
+    item.set("user", this.item.user);
+    item.set("size", this.item.size);
+    item.set("weight", this.item.weight);
+    item.set("value", this.item.value);
+    item.set("price", this.item.price);
 
 
+    var Sender = Parse.Object.extend("Senders");
+    var sender = new Sender();
+    sender.set("user", this.sender.user);
 
-        var Reciever = Parse.Object.extend("Recievers");
-        var reciever = new Reciever();
-        reciever.set("name", this.reciever.name);
-        reciever.set("phone", this.reciever.phone);
-        reciever.set("address", this.reciever.address);
+    sender.set("from", this.sender.from);
+    sender.set("to", this.sender.to);
+    sender.set("byWhen", this.sender.byWhen);
+    //sender.set("selected", true);
 
-        item.save(null, {
-          success: function(item) {
-            sender.set("item", item);
 
-            reciever.save(null, {
-              success: function(reciever) {
 
-                sender.set("reciever", reciever);
-                sender.save(null, {
-                  success: function(sender) {
-                    alert("Success!")
-                  },
-                  error: function(item, error) {
-                    console.log('Failed to create new object, with error code: ' + error.message);
-                  }
-                });
+    var Reciever = Parse.Object.extend("Recievers");
+    var reciever = new Reciever();
+    reciever.set("name", this.reciever.name);
+    reciever.set("phone", this.reciever.phone);
+    reciever.set("address", this.reciever.address);
+
+    item.save(null, {
+      success: function(item) {
+        sender.set("item", item);
+
+        reciever.save(null, {
+          success: function(reciever) {
+
+            sender.set("reciever", reciever);
+            sender.save(null, {
+              success: function(sender) {
+                alert("Success!")
               },
               error: function(item, error) {
                 console.log('Failed to create new object, with error code: ' + error.message);
@@ -152,88 +148,93 @@ flite.controller('Logistics', function ($rootScope){
             console.log('Failed to create new object, with error code: ' + error.message);
           }
         });
-    };
-
-    this.weightLabels = ["<2kg","2-5kg","5-10kg","10-16kg","16-23kg"];
-    this.sizeLabels = ["small items(phone, watch, etc)","medium-small(clothes, shoe, etc)",
-    "medium(bag, laptop, etc)","medium-large(carry-on, etc)","large(big luggage, etc)"];
-    this.valueLabels = ["<$500","$500-$1000","$1000-$1500","$1500-$2000","$2000-$2500"];
-
-    this.weightprice = function(){
-      if (this.item.weight == 1){
-        return 10;
-      }else if(this.item.weight == 2){
-        return 30;
-      }else if(this.item.weight == 3){
-        return 80;
-      }else if(this.item.weight == 4){
-        return 140;
-      }else if(this.item.weight == 5){
-        return 200;
+      },
+      error: function(item, error) {
+        console.log('Failed to create new object, with error code: ' + error.message);
       }
-    };
+    });
+  };
 
-    this.sizeprice = function(){
-      if(this.item.size == 1){
-        return 9.99;
-      }else if(this.item.size == 2){
-        return 19.99;
-      }else if(this.item.size == 3){
-        return 99.99;
-      }else if(this.item.size == 4){
-        return 150.99;
-      }else if(this.item.size == 5){
-        return 199.99;
-      }
-    };
+  this.weightLabels = ["<2kg","2-5kg","5-10kg","10-16kg","16-23kg"];
+  this.sizeLabels = ["small items(phone, watch, etc)","medium-small(clothes, shoe, etc)",
+  "medium(bag, laptop, etc)","medium-large(carry-on, etc)","large(big luggage, etc)"];
+  this.valueLabels = ["<$500","$500-$1000","$1000-$1500","$1500-$2000","$2000-$2500"];
 
-    this.valueprice = function(){
-      if(this.item.value == 1){
-        return 10;
-      }else if(this.item.value == 2){
-        return 20;
-      }else if(this.item.value == 3){
-        return 40;
-      }else if(this.item.value == 4){
-        return 70;
-      }else if(this.item.value == 5){
-        return 100;
-      }
-    };
+  this.weightprice = function(){
+    if (this.item.weight == 1){
+      return 10;
+    }else if(this.item.weight == 2){
+      return 30;
+    }else if(this.item.weight == 3){
+      return 80;
+    }else if(this.item.weight == 4){
+      return 140;
+    }else if(this.item.weight == 5){
+      return 200;
+    }
+  };
 
-    this.total = function(){
-      this.item.price = this.weightprice() + this.sizeprice() + this.valueprice()
-      return this.item.price;
-    };
+  this.sizeprice = function(){
+    if(this.item.size == 1){
+      return 9.99;
+    }else if(this.item.size == 2){
+      return 19.99;
+    }else if(this.item.size == 3){
+      return 99.99;
+    }else if(this.item.size == 4){
+      return 150.99;
+    }else if(this.item.size == 5){
+      return 199.99;
+    }
+  };
+
+  this.valueprice = function(){
+    if(this.item.value == 1){
+      return 10;
+    }else if(this.item.value == 2){
+      return 20;
+    }else if(this.item.value == 3){
+      return 40;
+    }else if(this.item.value == 4){
+      return 70;
+    }else if(this.item.value == 5){
+      return 100;
+    }
+  };
+
+  this.total = function(){
+    this.item.price = this.weightprice() + this.sizeprice() + this.valueprice()
+    return this.item.price;
+  };
 
 
-    var placeSearch, autocomplete, autocomplete2, autocomplete3;
+  var placeSearch, autocomplete, autocomplete2, autocomplete3;
 
-    this.initialize = function() {
-        autocomplete = new google.maps.places.Autocomplete(document.getElementById('autocomplete'),
-          { types: [ '(cities)' ] });
+  this.initialize = function() {
+    autocomplete = new google.maps.places.Autocomplete(document.getElementById('autocomplete'),
+      { types: [ '(cities)' ] });
 
-        google.maps.event.addListener(autocomplete, 'place_changed', function() {
-            var place = autocomplete.getPlace();
-            temp.sender.from = place.formatted_address;
-        });
+    google.maps.event.addListener(autocomplete, 'place_changed', function() {
+      var place = autocomplete.getPlace();
+      temp.sender.from = place.formatted_address;
+    });
 
-        autocomplete2 = new google.maps.places.Autocomplete(document.getElementById('autocomplete2'),
-          { types: [ '(cities)' ] });
+    autocomplete2 = new google.maps.places.Autocomplete(document.getElementById('autocomplete2'),
+      { types: [ '(cities)' ] });
 
-        google.maps.event.addListener(autocomplete2, 'place_changed', function() {
-            var place = autocomplete2.getPlace();
-            temp.sender.to = place.formatted_address;
-        });
+    google.maps.event.addListener(autocomplete2, 'place_changed', function() {
+      var place = autocomplete2.getPlace();
+      temp.sender.to = place.formatted_address;
+    });
 
-        autocomplete3 = new google.maps.places.Autocomplete(document.getElementById('autocomplete3'),
-          { types: [ 'geocode' ] });
+    autocomplete3 = new google.maps.places.Autocomplete(document.getElementById('autocomplete3'),
+      { types: [ 'geocode' ] });
 
-        google.maps.event.addListener(autocomplete3, 'place_changed', function() {
-            var place = autocomplete3.getPlace();
-            temp.reciever.address = place.formatted_address;
-        });
-    };
+    google.maps.event.addListener(autocomplete3, 'place_changed', function() {
+      var place = autocomplete3.getPlace();
+      temp.reciever.address = place.formatted_address;
+    });
+  };
 });
 
 
@@ -248,69 +249,70 @@ flite.controller('Deliver', function ($rootScope){
   var matches =[];
 
   this.save= function(){
-      var Deliverers = Parse.Object.extend("Deliverers");
-      var deliverer = new Deliverers();
+    var Deliverers = Parse.Object.extend("Deliverers");
+    var deliverer = new Deliverers();
 
-      deliverer.set("user", this.deliverer.user);
-      deliverer.set("sender", this.deliverer.sender);
-      deliverer.set("from", this.deliverer.from);
-      deliverer.set("to", this.deliverer.to);
-      deliverer.set("reciever", this.deliverer.reciever);
-      deliverer.set("dateOfTravel", this.deliverer.dateOfTravel);
+    deliverer.set("user", this.deliverer.user);
+    deliverer.set("sender", this.deliverer.sender);
+    deliverer.set("from", this.deliverer.from);
+    deliverer.set("to", this.deliverer.to);
+    deliverer.set("reciever", this.deliverer.reciever);
+    deliverer.set("dateOfTravel", this.deliverer.dateOfTravel);
 
-      deliverer.save(null, {
-        success: function(deliverer) {
-          var match = new Parse.Query("Senders");
+    deliverer.save(null, {
+      success: function(deliverer) {
+        var match = new Parse.Query("Senders");
 
-          match.equalTo("from", temp.deliverer.from);
-          match.equalTo("to", temp.deliverer.to);
-          match.greaterThanOrEqualTo("byWhen", temp.deliverer.dateOfTravel);
+        match.equalTo("from", temp.deliverer.from);
+        match.equalTo("to", temp.deliverer.to);
+        match.greaterThanOrEqualTo("byWhen", temp.deliverer.dateOfTravel);
+        //match.equalTo("selected", true);
 
-          match.find({
-            success: function(senders) {
-              if(senders.length == 0){
-                alert("Found no matches");
-              }
-              else{
-                for (var i= 0; i < senders.length; i++) {
-                  matches.push(senders[i].id);
-                }
-
-                localStorage["matches"] = JSON.stringify(matches);
-
-                window.location.href = "search.html";
-              }
-            },
-            error: function(error) {
-              alert("Error: " + error.code + " " + error.message);
+        match.find({
+          success: function(senders) {
+            if(senders.length == 0){
+              alert("Found no matches");
             }
-          });
-        },
-        error: function(deliverer, error) {
-          console.log('Failed to create new object, with error code: ' + error.message);
-        }
-      });
-  };
+            else{
+              for (var i= 0; i < senders.length; i++) {
+                matches.push(senders[i].id);
+              }
 
-  var placeSearch, autocomplete, autocomplete2;
+              localStorage["matches"] = JSON.stringify(matches);
 
-  this.initialize = function() {
-      autocomplete = new google.maps.places.Autocomplete(document.getElementById('autocomplete'),
-        { types: [ '(cities)' ] });
+              window.location.href = "search.html";
+            }
+          },
+          error: function(error) {
+            alert("Error: " + error.code + " " + error.message);
+          }
+        });
+      },
+      error: function(deliverer, error) {
+        console.log('Failed to create new object, with error code: ' + error.message);
+      }
+    });
+};
 
-      google.maps.event.addListener(autocomplete, 'place_changed', function() {
-          var place = autocomplete.getPlace();
-          temp.deliverer.from = place.formatted_address;
-      });
+var placeSearch, autocomplete, autocomplete2;
 
-      autocomplete2 = new google.maps.places.Autocomplete(document.getElementById('autocomplete2'),
-        { types: [ '(cities)' ] });
+this.initialize = function() {
+  autocomplete = new google.maps.places.Autocomplete(document.getElementById('autocomplete'),
+    { types: [ '(cities)' ] });
 
-      google.maps.event.addListener(autocomplete2, 'place_changed', function() {
-          var place = autocomplete2.getPlace();
-          temp.deliverer.to = place.formatted_address;
-      });
-  };
+  google.maps.event.addListener(autocomplete, 'place_changed', function() {
+    var place = autocomplete.getPlace();
+    temp.deliverer.from = place.formatted_address;
+  });
+
+  autocomplete2 = new google.maps.places.Autocomplete(document.getElementById('autocomplete2'),
+    { types: [ '(cities)' ] });
+
+  google.maps.event.addListener(autocomplete2, 'place_changed', function() {
+    var place = autocomplete2.getPlace();
+    temp.deliverer.to = place.formatted_address;
+  });
+};
 });
 
 flite.controller('Search', function ($rootScope, $scope){
@@ -352,21 +354,18 @@ flite.controller('Search', function ($rootScope, $scope){
 
                   },
                   error: function(object, error) {
-                    // The object was not retrieved successfully.
-                    // error is a Parse.Error with an error code and message.
+                    console.log('Failed to get object, with error code: ' + error.message);
                   }
                 });
 
               },
               error: function(object, error) {
-                // The object was not retrieved successfully.
-                // error is a Parse.Error with an error code and message.
+                console.log('Failed to get object, with error code: ' + error.message);
               }
             });
           },
           error: function(object, error) {
-            // The object was not retrieved successfully.
-            // error is a Parse.Error with an error code and message.
+            console.log('Failed to get object, with error code: ' + error.message);
           }
         });
       },
